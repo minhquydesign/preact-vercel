1810ae14d1d779b218c9b57627d9cf09e191ae87
MinhQuy <nampk095@gmail.com>
2021-10-28 10:37:29 +0200

    Update index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,4 +8,16 @@ const Home = () => (
   </div>
 );
 
+function Counter() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+      <div>Counter: {value}</div>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+      <button onClick={() => setValue(value - 1)}>Decrement</button>
+    </>
+  )
+}
+
 export default Home;
